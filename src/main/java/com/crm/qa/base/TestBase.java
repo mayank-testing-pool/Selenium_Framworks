3c47fa2a3a6eff4263b37ecb236e4b870635ff93
Maheshwari Mayank <MaheshwariMayank@JohnDeere.com>
2019-02-13 20:01:00 +0530

    Initial commit

---
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -0,0 +1,60 @@
+package com.crm.qa.base;
+
+import com.crm.qa.util.Util;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    static WebDriver driver;
+    static Properties prop;
+
+    public TestBase ()
+    {
+        try {
+            prop = new Properties();
+            FileInputStream fis = new FileInputStream( "C:\\Users\\mm18780\\Desktop\\Mayank\\Automation\\FreeCRMTest\\src\\main\\java\\com\\crm\\qa\\config\\config.properties" );
+            prop.load( fis );
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void initialization ()
+    {
+        String browsername = prop.getProperty( "browser" );
+        if (browsername.equals( "chrome" ))
+        {
+            System.setProperty("webdriver.chrome.driver","C:\\Users\\mm18780\\Desktop\\Mayank\\Automation\\Selenuim_Practice\\support\\chromedriver_2.41.exe");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--no-sandbox");
+            driver = new ChromeDriver(options);
+        }else if (browsername.equals( "firefox" ))
+        {
+            System.setProperty( "webdriver.gecko.driver","C:\\Users\\mm18780\\Desktop\\Mayank\\Automation\\Selenuim_Practice\\support\\geckodriver_0.23.exe" );
+            driver = new FirefoxDriver(  );
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout( Util.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Util.IMPLICITLY_WAIT, TimeUnit.SECONDS );
+        driver.get( prop.getProperty( "url" ) );
+
+
+    }
+}
---
+++ src/main/java/com/crm/qa/util/Util.java
@@ -0,0 +1,8 @@
+package com.crm.qa.util;
+
+public class Util {
+
+    public static long PAGE_LOAD_TIMEOUT = 20;
+    public static long IMPLICITLY_WAIT = 10;
+
+}
